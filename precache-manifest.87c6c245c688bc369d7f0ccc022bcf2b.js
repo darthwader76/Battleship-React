d7f62a1a4c27686ca71da142e291c02a615b1b93
Parth Doshi <doship2468@gmail.com>
2019-06-09 13:59:55 -0700

    Updates

---
+++ precache-manifest.87c6c245c688bc369d7f0ccc022bcf2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742931e2637108f4e81f241eec5ea66f",
+    "url": "//Battleship-React/build/index.html"
+  },
+  {
+    "revision": "b34ceb69a2e1c036d050",
+    "url": "//Battleship-React/build/static/css/main.b577b797.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "//Battleship-React/build/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "b34ceb69a2e1c036d050",
+    "url": "//Battleship-React/build/static/js/main.8edbcb67.chunk.js"
+  },
+  {
+    "revision": "f03597f000495d86ae60",
+    "url": "//Battleship-React/build/static/js/runtime~main.6708b74f.js"
+  }
+]);
